814f46f6fc5a9a50aa4d7041dab23409a332f829
Derek Hu <derek.dchu@gmail.com>
2014-12-04 15:54:34 -0500

    Create GameController.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,8 @@
 'use strict';
 
-var app = angular.module('2048App', []);
+var app = angular.module('2048App', ['Game']);
+
+// Game Controller
+app.controller('GameController', ['GameManager'], function(GameManager) {
+  this.game = GameManager;
+});
