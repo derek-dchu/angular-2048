6a1f0e8c4677399926fd0e61af11a99340d64d1b
Derek Hu <derek.dchu@gmail.com>
2014-12-04 15:55:14 -0500

    Create Game module.

---
+++ app/scripts/game/game.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('Game', [])
+.service('GameManager', function() {
+        // Create a new game
+        this.newGame = function() {};
+        // Handle user action
+        this.move = function() {};
+        // Update the score
+        this.updateScore = function() {};
+        // Check is there any move left
+        this.moveAvailable = function() {};
+    });
