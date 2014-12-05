ea9e2efed9da4047eafedc7c28ab6289acffe2db
Derek Hu <derek.dchu@gmail.com>
2014-12-05 00:58:11 -0500

    Create unit test for reinit, updateScore methods.

--- test/unit/game/game_spec.js
+++ test/unit/game/game_spec.js
@@ -5,9 +5,9 @@ describe('Game module', function() {
 
         var provide;
 
-        // Create mocked gridService for this test
         var _gridService;
         beforeEach(module(function($provide) {
+            // Create mocked gridService for this test
             _gridService = {
                 anyCellsAvailable: angular.noop,
                 tileMatchesAvailable: angular.noop
@@ -15,6 +15,7 @@ describe('Game module', function() {
 
             // Replace real GridService with mocked one
             $provide.value('GridService', _gridService);
+
             provide = $provide;
         }));
 
@@ -25,6 +26,32 @@ describe('Game module', function() {
 
         it('should have a GameManage', function() {
             expect(gameManager).toBeDefined();
+        });
+
+        describe('.reinit', function() {
+            beforeEach(function() {
+                gameManager.reinit();
+            });
+
+            it('should initialize current score to 0', function() {
+                   expect(gameManager.currentScore).toEqual(0);
+            });
+            it('should initialize best score to 0', function() {
+                    expect(gameManager.bestScore).toEqual(0);
+            });
+        });
+
+        describe('.updateScore', function() {
+            it('should update current score', function() {
+                gameManager.updateScore(1);
+                expect(gameManager.currentScore).toEqual(1);
+            });
+
+            it('should update best score when new score is higher than best score', function() {
+                gameManager.bestScore = 10;
+                gameManager.updateScore(50);
+                expect(gameManager.getBestScore()).toEqual(50);
+            });
         });
 
         describe('.movesAvailable', function() {
