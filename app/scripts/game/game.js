cb14db8e0d8142e1c313e1a0fbd3fbd4791772c6
Derek Hu <derek.dchu@gmail.com>
2014-12-05 00:59:04 -0500

    Create reinit, updateScore and getBestScore methods. Pass unit test.

--- app/scripts/game/game.js
+++ app/scripts/game/game.js
@@ -1,15 +1,32 @@
 'use strict';
 
-angular.module('Game', [])
+angular.module('Game', ['Grid'])
+
 .service('GameManager', ['GridService', function(GridService) {
-        // Create a new game
-        this.newGame = function() {};
-        // Handle user action
-        this.move = function() {};
-        // Update the score
-        this.updateScore = function() {};
-        // Check is there any move left
-        this.moveAvailable = function() {
-                return GridService.anyCellsAvailable() || GridService.tileMatchesAvailable();
-        };
-    }]);
+    // initialize service
+    this.reinit = function() {
+        this.currentScore = 0;
+        this.bestScore = 0;
+    };
+    this.reinit();
+
+    // Create a new game
+    this.newGame = function() {};
+    // Handle user action
+    this.move = function() {};
+    // Update the score
+    this.updateScore = function(newScore) {
+        this.currentScore = newScore;
+        if (this.currentScore > this.bestScore) {
+            this.bestScore = this.currentScore;
+        }
+    };
+    // Check is there any move left
+    this.moveAvailable = function() {
+        return GridService.anyCellsAvailable() || GridService.tileMatchesAvailable();
+    };
+    // Get best score
+    this.getBestScore = function() {
+        return this.bestScore;
+    };
+}]);
