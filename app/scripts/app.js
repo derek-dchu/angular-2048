307408911df768828f5e290977a604a59c3a512a
Derek Hu <derek.dchu@gmail.com>
2014-12-08 01:00:00 -0500

    Refactor GameController. Force angular to digest after keyboard event. Fix display issue.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,23 +3,23 @@
 var app = angular.module('2048App', ['Game', 'Grid', 'Keyboard']);
 
 // Game Controller
-app.controller('GameController', ['GameManager', 'KeyboardManager', function(GameManager, KeyboardManager) {
-  KeyboardManager.init();
+app.controller('GameController', ['$scope', 'GameManager', 'KeyboardManager', function($scope, GameManager, KeyboardManager) {
   this.gameManager = GameManager;
 
   this.newGame = function() {
+    KeyboardManager.init();
     this.gameManager.newGame();
+    this.startGame();
   };
-
-  this.newGame();
 
   // Start game by binding key event to the game
   this.startGame = function() {
     var self = this;
     KeyboardManager.on(function(key) {
       self.gameManager.move(key);
+      $scope.$apply();
     });
   };
 
-  this.startGame();
+  this.newGame();
 }]);
