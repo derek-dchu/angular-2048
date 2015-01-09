40ae6e4169a3935becc74216d304e85fa03f79d0
Derek Hu <derek.dchu@gmail.com>
2015-01-08 21:06:23 -0500

    Update dist js files.

--- dist/js/angular-2048.js
+++ dist/js/angular-2048.js
@@ -1,47 +1,6 @@
 'use strict';
 
-var app = angular.module('2048App', ['Game', 'Grid', 'Keyboard']);
-
-// Save templates
-app.run(['$templateCache', function($templateCache) {
-  $templateCache.put('mainId.html',
-    '<div id="content" ng-controller="GameController as gctrl">' +
-    '<div class="row">' +
-    '<div id="game-board" class="col-md-9">' +
-    '<div grid ng-model="gctrl.gameManager"></div> ' +
-    '</div>' +
-    '<div id="user-board" class="col-md-3">' +
-    '<div class="score">' +
-    '<p>Current Score: {{ gctrl.gameManager.currentScore }}</p>' +
-    '</div>' +
-    '<div class="best-score">' +
-    '<p>Best Score: {{ gctrl.gameManager.bestScore }}</p>' +
-    '</div>' +
-    '</div>' +
-    '</div>' +
-    '</div>'
-  );
-
-  $templateCache.put('gridId.html',
-    '<div class="grid-container">' +
-    '<div class="grid-cell"' +
-    'ng-repeat="cell in ngModel.grid track by $index"></div>' +
-    '</div>' +
-    '<div class="tile-container">' +
-    '<div tile ng-model="tile" ng-repeat="tile in ngModel.tiles track by $id(tile.id || $index)"></div>' +
-    '</div>'
-  );
-
-  $templateCache.put('tileId.html',
-    '<div ng-if="ngModel" class="tile tile-{{ ngModel.value }} position-{{ ngModel.coordinate.x }}-{{ ngModel.coordinate.y }}">' +
-    '<div class="tile-inner">' +
-    '{{ ngModel.value }}' +
-    '</div>' +
-    '</div>'
-  );
-}]);
-
-
+var app = angular.module('2048App', ['Game', 'Grid', 'Keyboard', 'templates']);
 
 // Game Controller
 app.controller('GameController', ['GameManager', 'KeyboardManager', function(GameManager, KeyboardManager) {
